0b14937d82d8a830f3856746945391aa8a29195a
tangrufei <1042731605@qq.com>
2026-01-14 14:42:40 +0800

    [fix]文件补充提交

--- tt-admin-backend/tt-admin-plugins/tt-admin-ai-chat/ui/src/App.vue
+++ tt-admin-backend/tt-admin-plugins/tt-admin-ai-chat/ui/src/App.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="plugin-container ai-chat-app">
-    <AiChatView />
-  </div>
+  <AiChatView />
 </template>
 
 <script setup lang="ts">
 import AiChatView from './modules/ai-chat/view/AiChatView.vue';
-import './style.css';
 </script>
